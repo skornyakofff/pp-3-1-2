5920756fa3a3cbecc93f23f960d2f6d47d45c08d
skornyakofff <badabyyym@yandex.ru>
2022-09-21 09:11:50 +0500

    fix

--- src/main/java/ru/kata/spring/boot_security/demo/repository/RoleRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repository/RoleRepository.java
@@ -4,9 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.kata.spring.boot_security.demo.models.Role;
 
-import javax.transaction.Transactional;
-
 @Repository
-@Transactional
 public interface RoleRepository extends JpaRepository<Role, Integer> {
 }
